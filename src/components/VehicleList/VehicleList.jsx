d2dac9d7200899818eed0313c458a3313bcb84b3
Luke <luke.michals@gmail.com>
2018-03-01 19:32:52 -0800

    add antd component library and render button to test

--- src/components/VehicleList/VehicleList.jsx
+++ src/components/VehicleList/VehicleList.jsx
@@ -4,12 +4,10 @@ import React, { Component } from "react";
 // css imports
 import "./VehicleList.css";
 
-class VehicleList extends Component {
-  constructor(props) {
-    super(props);
-    // console.log({ props });
-  }
+// component imports
+import Button from "antd/lib/button";
 
+class VehicleList extends Component {
   componentDidMount() {
     const { dispatch, fetchVehicleListAndUpdateState } = this.props;
     dispatch(fetchVehicleListAndUpdateState());
@@ -18,6 +16,7 @@ class VehicleList extends Component {
   render() {
     return (
       <div>
+        <Button type="primary">Button</Button>
         {this.props.list.map((v, i) =>
           <div key={i}>
             {v.make}
