d71754b78965f350cd5c7b6aa18021470da9c713
Luke <luke.michals@gmail.com>
2018-03-02 18:30:52 -0800

    Add grid for responsive design

--- src/components/ControlPanel/ControlPanel.jsx
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -2,34 +2,54 @@
 import React from "react";
 
 // component imports
-import { Input, Button, Card } from "antd";
+import { Input, Button, Card, Row, Col } from "antd";
+// const ButtonGroup = Button.Group;
 
 const ControlPanel = ({ actions }) => {
   const { sortVehicleList, filterListByString } = actions;
   return (
     <Card className="app-item">
-      <Input
-        placeholder="Search by year, make or model"
-        onPressEnter={e => filterListByString(e.target.value)}
-      />
-      <Button
-        type="primary"
-        onClick={() => sortVehicleList("year", "ascending")}
-      >
-        Sort by vehicle year (oldest to newest)
-      </Button>
-      <Button
-        type="primary"
-        onClick={() => sortVehicleList("mileage", "descending")}
-      >
-        Sort by mileage (highest to lowest)
-      </Button>
-      <Button
-        type="primary"
-        onClick={() => sortVehicleList("created_at", "descending")}
-      >
-        Sort by listing date (newest to oldest)
-      </Button>
+      <Row type="flex" justify="space-around" align="middle" gutter={16}>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <Input
+            placeholder="Search by year, make or model"
+            onPressEnter={e => filterListByString(e.target.value)}
+          />
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <Row>
+            <Col>
+              <span>Sort By: </span>
+            </Col>
+            <Col>
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => sortVehicleList("year", "ascending")}
+                style={{ margin: "5px" }}
+              >
+                Year (oldest to newest)
+              </Button>
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => sortVehicleList("mileage", "descending")}
+                style={{ margin: "5px" }}
+              >
+                Mileage (highest to lowest)
+              </Button>
+              <Button
+                size="small"
+                type="primary"
+                onClick={() => sortVehicleList("created_at", "descending")}
+                style={{ margin: "5px" }}
+              >
+                Listing date (newest to oldest)
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </Card>
   );
 };
